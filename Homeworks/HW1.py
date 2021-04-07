7bcbad9d91ee486cce6657226fadb4d0c832d78d
Musa DEMİRKESEN <81981004+mdemirkesen@users.noreply.github.com>
2021-04-08 00:06:49 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,15 @@
-#Explain your work
 
+#Listenin ilk dört elemanı ile son dört elemanının yerlerini değiştiriyor.
 #Question 1
-for x in range(a):
-	print(a)
+myList = [1,2,3,4,5,6,7,8]
+part1=myList[:4]
+part2=myList[4:]
+myList = part2 + part1
+print(myList)
+
+#Girilen "n" sayısına kadar çift sayıları ekrana yazdırıyor.
+#Question 2
+num = int(input("Dizinin son sayısını giriniz: "))
+for i in range(1,num+1):
+    if i % 2 == 0:
+        print(i)
