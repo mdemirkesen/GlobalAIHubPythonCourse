5a9da2e4b8c3009827cc20925e8eebc1b847852d
Musa DEMİRKESEN <81981004+mdemirkesen@users.noreply.github.com>
2021-04-08 00:12:12 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,33 @@
-#Explain your work
+#Girilen kullanıcı adı ve parola bilgisini sistemte kayıtlı olan kullanıcı adı ve parola ile karşılaştırıyor.
+#Sağlanan koşula göre kullanıcı adı / parola / kullanıcı ado - parola hatalı çıktısını üretiyor. 
+#Bunların dışındaki durumda kullanıcı adı ve parola doğru olacağından giriş başarılı çıktısı üretiliyor..
 
 #Question 1
-for x in range(a):
-	print(a)
+sysUserName = "user"
+sysPassword = "python"
+
+userName = input("Kullanıcı Adı: ")
+password = input("Parola: ")
+
+if sysUserName != userName and sysPassword != password:
+    print("Giriş başarısız. \nKullanıcı adı ve parola hatalı.")
+elif sysUserName == userName and sysPassword != password:
+    print("Giriş başarısız. \nParola hatalı")
+elif sysUserName != userName and sysPassword == password:
+    print("Giriş başarısız. \nKullanıcı adı hatalı.")
+else:
+    print("Giriş başarlı.")
+
+#Alternatif Çözüm
+userInfo ={"sysUserName":"user","sysPassword":"python"}
+userName = input("Kullanıcı Adı: ")
+password = input("Parola: ")
+
+if userInfo["sysUserName"] != userName and userInfo["sysPassword"] != password:
+    print("Giriş başarısız. \nKullanıcı adı ve parola hatalı.")
+elif userInfo["sysUserName"] == userName and userInfo["sysPassword"] != password:
+    print("Giriş başarısız. \nParola hatalı")
+elif userInfo["sysUserName"] != userName and userInfo["sysPassword"] == password:
+    print("Giriş başarısız. \nKullanıcı adı hatalı.")
+else:
+    print("Giriş başarlı.")
